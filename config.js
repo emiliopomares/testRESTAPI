9a00aacefb0e263c8f82cae60e5392351a9b4c3e
Emilio Pomares <remilia@Emilios-iMac.local>
2018-12-18 11:29:22 +0100

    Background workers

--- config.js
+++ config.js
@@ -5,7 +5,12 @@ environments.staging = {
 	'httpsPort': 3001,
 	'envName': 'staging',
 	'hashingSecret': 'thisIsAStagingSecret',
-	'maxChecks' : 5
+	'maxChecks' : 5,
+	'twilio' : {
+		'accountSid' : 'ACb32d411ad7fe886aac54c665d2e5c5d',
+		'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+		'fromPhone' : '+15005550006'
+	}
 }
 
 environments.production = {
--- index.js
+++ index.js
@@ -2,111 +2,16 @@
  *
  */
 
-const http = require('http')
-const https = require('https')
-const url = require('url')
-const StringDecoder = require('string_decoder').StringDecoder
-const config = require('./config')
-const fs = require('fs')
-const _data = require('./lib/data')
-const handlers = require('./lib/handlers')
-const helpers = require('./lib/helpers')
+ const server = require('./lib/server')
+ const workers = require('./lib/workers')
 
-const certFile = fs.readFileSync('./ssl/cert.pem')
-const keyFile = fs.readFileSync('./ssl/key.pem')
+ var app = {}
 
-const credentials = 
-{
-	cert: certFile,
-	key: keyFile
-}
+ app.init = function() {
+        server.init()
+        workers.init()
+ }
 
-function serverFunction(req, res) {
+ app.init()
 
-	var parsedURL = url.parse(req.url, true)
-
-        var path = parsedURL.pathname
-        var trimmedPath = path.replace(/^\/+|\/+$/g, '')
-
-
-        var queryStringObject = parsedURL.query;
-
-        console.log("query: " + JSON.stringify(queryStringObject))
-
-
-        var components = trimmedPath.split('/')
-
-
-        var method = req.method.toLowerCase()
-
-
-        var headers = req.headers;
-        console.log("Headers are: ", headers);
-
-	if(components[0] == "service") {
-                console.log("Service requested");
-        }
-        else {
-                console.log("File requested");
-        }
-
-
-        var decoder = new StringDecoder('utf-8')
-        var buffer = ''
-        req.on('data',function(data) {
-                buffer += decoder.write(data)
-        })
-        req.on('end',function() {
-                buffer += decoder.end()
-
-                var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-		console.log("typeof:" + typeof(router[trimmedPath]))
-                var data = {
-                        'trimmedPath' : trimmedPath,
-                        'queryStringObject' : queryStringObject,
-                        'method' : method,
-                        'headers' : headers,
-                        'payload' : helpers.parseJsonToObject(buffer)
-                }
-
-		chosenHandler(data, function(statusCode, payload) {
-                        statusCode = typeof(statusCode) == 'number' ? statusCode : 200
-                        payload = typeof(payload) == 'object' ? payload : {}
-                        var payloadString = JSON.stringify(payload)
-                        res.setHeader('Content-Type', 'application/json')
-                        res.writeHead(statusCode)
-                        res.end(payloadString)
-                })
-
-
-
-        })
-
-
-
-}
-
-
-const httpServer = http.createServer(serverFunction)
-const httpsServer = https.createServer(credentials, serverFunction)
-
-httpServer.listen(config.httpPort, function() {
-
-	console.log('http server started on port ' + config.httpPort + ' in ' + config.envName + ' mode')
-
-})
-
-httpsServer.listen(config.httpsPort, function() {
-
-	console.log('https server started on port ' + config.httpsPort + ' in ' + config.envName + ' mode')
-
-})
-
-
-var router = {
-	'sample' : handlers.sample,
-	'ping': handlers.ping,
-	'users': handlers.users,
-	'checks': handlers.checks,
-	'tokens' : handlers.tokens
-}
+ module.exports = app
